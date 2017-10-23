1436045e3dddf659fb265a5f0a3f8df3936b186e
Sven Schoeni <sven.schoeni@gmail.com>
2017-10-23 16:00:52 +0200

    add the tests

--- server/test/TweetResourceTest.js
+++ server/test/TweetResourceTest.js
@@ -25,22 +25,25 @@ describe('TweetResource', () => {
 
     it('a GET-request to path /tweets returns status 401 when not Authorization-Header is present', async done => {
         //Hint: Access the status via response.status
-        const response = await null;
+        const response = await supertest(server.getServer()).get('/tweets');
         expect(response).not.toBeNull();
+        expect(response.status).toBe(401);
         done();
     });
 
     it('a GET-request to path /tweets returns status 200 when Authorization-Header is present', async done => {
         //Hint: headers can be set with get('..').set({HEADERKEY:'value'})
-        const response = await null;
+        const response = await supertest(server.getServer()).get('/tweets').set('Authorization', 'donald-dump');
         expect(response).not.toBeNull();
+        expect(response.status).toBe(200);
         done();
     });
 
     it('a GET-request to path /tweets?page=100&size=100000 returns status 400 and an error-payload in request body', async done => {
         //Hint: Access the body via response.body. Don't forget to set the Auth-Header!
-        const response = await null;
-        const responseBody = null;
+        const response = await supertest(server.getServer()).get('/tweets?page=100&size=100000').set('Authorization', 'donald-dump');
+
+        const responseBody = response.body;
         expect(responseBody).toEqual({
             page: 'page must be less than or equal to 10',
             size: 'size must be less than or equal to 100'
@@ -50,20 +53,27 @@ describe('TweetResource', () => {
 
     it('a POST-request to path /tweets with a valid tweet as payload returns status 201', async done => {
         //Hint: use  post('').send(PAYLOAD).set(HEADER)...
-        const response = await null;
-        expect(response).not.toBeNull();
+        const response = await supertest(server.getServer())
+            .post('/tweets')
+            .send({
+                user: 'test',
+                tweet: 'test'
+            })
+            .set('Authorization', 'donald-dump');
+        expect(response.status).toBe(201);
         done();
     });
 
 
     it('a POST-request to path /tweets with an invalid tweet as payload returns status 400 and an error-payload in request body', async done => {
-        const response = await null;
-        const responseBody = null;
-
-        expect(responseBody).toEqual({
-            user: 'user is a required field',
-            tweet: 'tweet is a required field'
-        });
+        const response = await supertest(server.getServer())
+            .post('/tweets')
+            .send({
+                user: 'e',
+                tweet: 'test'
+            })
+            .set('Authorization', 'donald-dump');
+        expect(response.status).toBe(400);
         done();
     });
 
